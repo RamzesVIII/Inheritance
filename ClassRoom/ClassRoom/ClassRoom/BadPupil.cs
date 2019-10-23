c59eae241049780179d21e555d41c3531917d24a
Roman <litl_rom@tut.by>
2019-10-23 19:00:55 +0300

    Derrived classes was added

---
+++ ClassRoom/ClassRoom/ClassRoom/BadPupil.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    class BadPupil : Pupil
+    {
+        public override void Study()
+        {
+            Console.WriteLine("Bad pupil study");
+        }
+        public override void Read()
+        {
+            Console.WriteLine("Bad pupil read");
+        }
+        public override void Write()
+        {
+            Console.WriteLine("Bad pupil write");
+        }
+        public override void Relax()
+        {
+            Console.WriteLine("Bad pupil relax");
+        }
+    }
+}
