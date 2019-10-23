4de7dd7d3b7bc2e43dd7b6a3f6ec9f1bbe7307e4
Roman <litl_rom@tut.by>
2019-10-23 21:23:57 +0300

    ClassRoom complete

--- ClassRoom/ClassRoom/ClassRoom/ClassRoom.cs
+++ ClassRoom/ClassRoom/ClassRoom/ClassRoom.cs
@@ -8,6 +8,66 @@ namespace ClassRoom
 {
     class ClassRoom
     {
+        Pupil pupil_1, pupil_2, pupil_3, pupil_4;
+        int counter;
+        public ClassRoom (Pupil pupil_1, Pupil pupil_2, Pupil pupil_3, Pupil pupil_4)
+        {
+            this.pupil_1 = pupil_1;
+            this.pupil_2 = pupil_2;
+            this.pupil_3 = pupil_3;
+            this.pupil_4 = pupil_4;
+            counter = 4;
+        }
 
+        public ClassRoom (Pupil pupil_1, Pupil pupil_2, Pupil pupil_3)
+        {
+            this.pupil_1 = pupil_1;
+            this.pupil_2 = pupil_2;
+            this.pupil_3 = pupil_3;
+            counter = 3;
+        }
+        public ClassRoom(Pupil pupil_1, Pupil pupil_2)
+        {
+            this.pupil_1 = pupil_1;
+            this.pupil_2 = pupil_2;
+            counter = 2;
+        }
+
+        public void Show ()
+        {
+            if (counter >= 2)
+            {
+                pupil_1.Study();
+                pupil_1.Read();
+                pupil_1.Write();
+                pupil_1.Relax();
+                Console.WriteLine(new string('-', 40));
+
+                pupil_2.Study();
+                pupil_2.Read();
+                pupil_2.Write();
+                pupil_2.Relax();
+                Console.WriteLine(new string('-', 40));
+
+                if (counter >= 3)
+                {
+                    pupil_3.Study();
+                    pupil_3.Read();
+                    pupil_3.Write();
+                    pupil_3.Relax();
+                    Console.WriteLine(new string('-', 40));
+
+                    if (counter >= 4)
+                    {
+                        pupil_4.Study();
+                        pupil_4.Read();
+                        pupil_4.Write();
+                        pupil_4.Relax();
+                        Console.WriteLine(new string('-', 40));
+                    }
+                }
+            }
+            
+        }
     }
 }
--- ClassRoom/ClassRoom/ClassRoom/Program.cs
+++ ClassRoom/ClassRoom/ClassRoom/Program.cs
@@ -10,6 +10,20 @@ namespace ClassRoom
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("firstclass");
+            Console.WriteLine(new string('-', 70));
+            ClassRoom firstclass = new ClassRoom(new BadPupil (), new GoodPupil());
+            firstclass.Show();
+
+            Console.WriteLine("secondclass");
+            Console.WriteLine(new string ('-', 70));
+            ClassRoom secondclass = new ClassRoom(new BadPupil(), new GoodPupil(), new ExcelentPupil());
+            secondclass.Show();
+
+            Console.WriteLine("thirdclass");
+            Console.WriteLine(new string('-', 70));
+            ClassRoom thirdclass = new ClassRoom(new GoodPupil(), new GoodPupil(), new GoodPupil(), new GoodPupil());
+            thirdclass.Show();
         }
     }
 }
