bda1c5f4a75957f4dad284a58396930cbbf25f10
Roman <litl_rom@tut.by>
2019-10-25 19:15:18 +0300

    Derived Classes was added

---
+++ DocumentWorker/DocumentWorker/DocumentWorker/DocumentWorker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentWorker
+{
+    class DocumentWorker
+    {
+        public virtual void OpenDocument ()
+        {
+            Console.WriteLine("Документ открыт");
+        }
+        public virtual void EditDocument ()
+        {
+            Console.WriteLine("Редактирование документа доступно в версии Pro");
+        }
+        public virtual void SaveDocument ()
+        {
+            Console.WriteLine("Сохранение документа доступно в версии Pro");
+        }
+    }
+}
---
+++ DocumentWorker/DocumentWorker/DocumentWorker/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentWorker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DocumentWorker freeversion = new DocumentWorker();
+            freeversion.OpenDocument();
+            freeversion.EditDocument();
+            freeversion.SaveDocument();
+            Console.WriteLine("Вы пользуетесь бесплатной версией продукта, если хотите пользоваться версией Pro или Expert" +
+                "введите соответствующий ключ : pro или expert ");
+
+            if (Console.ReadLine() == "pro")
+            {
+                DocumentWorker proversion = new ProDocumentWorker();
+                proversion.OpenDocument();
+                proversion.EditDocument();
+                proversion.SaveDocument();
+            }
+            else if (Console.ReadLine () == "expert")
+            {
+                DocumentWorker expertversion = new ExpertDocumentWorker();
+                expertversion.OpenDocument();
+                expertversion.EditDocument();
+                expertversion.SaveDocument();
+            }
+            else if ()
+            {
+
+            }
+        }
+    }
+}
